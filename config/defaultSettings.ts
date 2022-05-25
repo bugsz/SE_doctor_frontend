d11da9a0f1881941dad97f02c56f5d3271a000f6
Locietta <locietta@qq.com>
2022-05-25 22:49:00 +0800

    fix: 更新了登陆后左上角的图标

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -14,7 +14,7 @@ const Settings: LayoutSettings & {
   colorWeak: false,
   title: '医生管理系统',
   pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/logo/logo2.svg',
   iconfontUrl: '',
 };
 
