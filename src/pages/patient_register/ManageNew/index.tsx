4fbf6f488b60e3f40e1bb25fa86ba8cf4f31c1ca
DH-Cang <cdh573885@outlook.com>
2022-06-04 23:37:04 +0800

    fix(patient_register): format update

--- src/pages/patient_register/ManageNew/index.tsx
+++ src/pages/patient_register/ManageNew/index.tsx
@@ -1,10 +1,11 @@
 import type { ActionType, ProColumns } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
 import { Button, DatePicker } from 'antd';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import request from 'umi-request';
 import type { patientItem } from './data.d';
+import { GetDoctorId, GetPatientRegisterList } from './service';
 
 function getPatientInfoUrl(record: patientItem) {
   return `/patient_register/diagnosis/${record.patient_id}`;
@@ -56,12 +57,17 @@ let curr_date = '';
 const tmp_depart = 'abcd';
 
 function onChange(date, dateString) {
-  console.log(date, dateString);
-  curr_date = dateString;
+  curr_date = dateString.replace(/[-_]/g, "");
+  console.log(curr_date);
 }
 
 const ManageNew = () => {
   const actionRef = useRef<ActionType>();
+  const response_me_id = GetDoctorId({}).then(res => {
+    return res.data.id;
+  })
+  console.log(response_me_id);
+
   return (
     <div>
       <DatePicker onChange={onChange} picker="day" />,
@@ -70,17 +76,36 @@ const ManageNew = () => {
         actionRef={actionRef}
         cardBordered
         // TODO: add current user's department; current date
-        request={async (params = { date: curr_date, department: tmp_depart }, sort, filter) => {
-          //params = {date : curr_date, department : tmp_depart}
-          // console.log(sort, filter);
-          // console.log(params);
-          // console.log(params.name);
-          return request<{
-            data: patientItem[];
-          }>('/api/register/get', {
-            params,
-          });
-        }}
+        // request={async (params = { date: curr_date, doctor_id: response_me_id }, sort, filter) => {
+        //   //params = {date : curr_date, department : tmp_depart}
+        //   // console.log(sort, filter);
+        //   // console.log(params);
+        //   // console.log(params.name);
+        //   return request<{
+        //     data: patientItem[];
+        //   }>('/api/register/get', {
+        //     params,
+        //   });
+        params = { params }
+        request= {
+          async (params = {date: curr_date, doctor_id: response_me_id}, sort, filter) => {
+            console.log(params)
+            console.log()
+            const response = await GetPatientRegisterList(params).then(res => {
+              console.log(params)
+              console.log(res.data)
+              let item = []
+              for(var i=0; i<res.data.length; i++){
+                item[i].patient_id = res.data.patient_id;
+                item[i].patient_name = res.data.name;
+                item[i].register_time = res.data.time_slice;
+              }
+              return item;
+            })
+            return Promise.resolve(response)
+          }
+        }
+
         editable={{
           type: 'single',
         }}
--- src/pages/patient_register/ManageNew/service.ts
+++ src/pages/patient_register/ManageNew/service.ts
@@ -1,15 +1,32 @@
 import { request } from 'umi';
 
-export async function ListDoctor(params) {
-  return request("/api/doctor/get", {
+export async function GetDoctorId(params) {
+  return request("/api/me", {
     method: "GET",
     params: { ...params },
   })
 }
-export async function DeleteDoctor(params) {
-  return request("/api/doctor/delete", {
-    method: "DELETE",
+
+export async function GetPatientRegisterList(params) {
+  return request("/api/register/get", {
+    method: "GET",
     params: {...params},
-  }
-  );
+  });
 }
+
+// export async function ListDoctor(params) {
+//   return request("/api/doctor/get", {
+//     method: "GET",
+//     params: { ...params },
+//   })
+// }
+// export async function DeleteDoctor(params) {
+//   return request("/api/doctor/delete", {
+//     method: "DELETE",
+//     params: {...params},
+//   }
+//   );
+// }
+
+
+
